1b66c2fa21cbc991da30d6eecb42192c75d2f236
Valeriu Olariu <ovaleriu7@gmail.com>
2019-05-12 21:17:53 +0100

    Adding Custom TensFlow Data set model

--- app/src/main/java/com/example/catid/MainActivity.kt
+++ app/src/main/java/com/example/catid/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.catid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ml.common.modeldownload.FirebaseLocalModel
 import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions
 import com.google.firebase.ml.common.modeldownload.FirebaseModelManager
 import com.google.firebase.ml.common.modeldownload.FirebaseRemoteModel
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +24,10 @@ class MainActivity : AppCompatActivity() {
             .setUpdatesDownloadConditions(conditions)
             .build()
         FirebaseModelManager.getInstance().registerRemoteModel(remoteModel)
+
+        val localModel = FirebaseLocalModel.Builder("my_local_model")
+            .setAssetFilePath("manifest.json")
+            .build()
+        FirebaseModelManager.getInstance().registerLocalModel(localModel)
     }
 }
